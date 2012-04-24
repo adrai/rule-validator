1b442e74c123137a553272331875eda496351863
Adriano Raiano <araiano@ksw.kaba.com>
2012-04-24 16:39:03 +0200

    first commit

---
+++ README.markdown
@@ -0,0 +1,68 @@
+# Introduction
+
+[![Build Status](https://secure.travis-ci.org/adrai/rule-validator.png)](http://travis-ci.org/adrai/rule-validator)
+
+Rule-validator is a javascript module based on amanda. And works for node.js and in the browser.
+It can be very useful if you work with (d)ddd, cqrs, eventsourcing, domain, commands and events, etc.
+
+# Installation
+
+    $ npm install rule-validator
+
+# Usage
+
+	var personValRules = base.extend({
+
+        createPerson: {
+            lastName: { type: 'string', minLength: 1 }
+        },
+
+        renamePerson: function() {
+            return this.schema(
+                this.attributes.firstName.isRequired(),
+                this.attributes.lastName.isRequired()
+            );
+        }
+
+    },
+    {
+        // optional attributes that can be used in rules...
+        attributes: {
+            firstName: { type: 'string', minLength: 1 },
+            lastName: { type: 'string', minLength: 1 }
+        },
+
+        // own stuff...
+        aggregate: 'personAggregate'
+    });
+
+    // validate...
+    personValRules.validate('createPerson', { lastName: 'Fritz' }, function(err) {
+        
+    });
+    // or...
+    personValRules.createPerson.validate({ lastName: 'Fritz' }, function(err) {
+    });
+
+
+# License
+
+Copyright (c) 2012 Adriano Raiano
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ test/ruleValidatorTest.js
@@ -0,0 +1,91 @@
+var expect = expect || require('expect.js')
+  , base = this.ruleValidator || require('../lib/ruleValidator');
+
+describe('Rule-Validator', function() {
+
+    describe('extending ruleValidator', function() {
+
+        var personValRules = base.extend({
+
+            createPerson: {
+                lastName: { type: 'string', minLength: 1 }
+            },
+
+            renamePerson: function() {
+                return this.schema(
+                    this.attributes.firstName.isRequired(),
+                    this.attributes.lastName.isRequired()
+                );
+            }
+
+        },
+        {
+            attributes: {
+                firstName: { type: 'string', minLength: 1 },
+                lastName: { type: 'string', minLength: 1 }
+            },
+
+            aggregate: 'personAggregate'
+        });
+
+        it('it should contain all extending values', function() {
+            expect(personValRules.aggregate).to.eql('personAggregate');
+        });
+
+        describe('calling validate on the new object', function() {
+
+            it('it should validate correctly', function() {
+                personValRules.validate('createPerson', { lastName: 'Fritz' }, function(err) {
+                    expect(err).not.to.be.ok();
+                });
+            });
+
+        });
+
+        describe('calling validate on the rule', function() {
+
+            it('it should validate correctly', function() {
+                personValRules.createPerson.validate({ lastName: 'Fritz' }, function(err) {
+                    expect(err).not.to.be.ok();
+                });
+            });
+
+        });
+
+        describe('with rules as objects', function() {
+
+            it('it should wrap the rules correctly', function() {
+                expect(personValRules.createPerson.properties.lastName.type).to.eql('string');
+            });
+
+        });
+
+        describe('with rules as functions', function() {
+
+            it('it should save its executed value and wrap the rules correctly', function() {
+                expect(personValRules.renamePerson.properties.firstName.type).to.eql('string');
+            });
+
+            it('it should have a schema function', function() {
+                var res = personValRules.schema({ a: 't' }, { b: 'c'});
+                expect(res.a).to.eql('t');
+                expect(res.b).to.eql('c');
+            });
+
+            describe('having extended with attributes', function() {
+
+                it('it should have saved the attributes correctly to can be used in the rules', function() {
+                    expect(personValRules.attributes.lastName.lastName.type).to.eql('string');
+                    var res = personValRules.attributes.lastName.isRequired();
+                    expect(res.lastName.required).to.eql(true);
+                    var res2 = personValRules.attributes.lastName.isNotRequired();
+                    expect(res2.lastName.required).to.eql(false);
+                });
+
+            });
+
+        });
+
+    });
+
+});
